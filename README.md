1a79b168dd32f64259ee4ecf635099be49fb563d
turbo <615951217@qq.com>
2016-10-19 16:41:18 +0800

    添加第三种方式，修改第二种方式

---
+++ README.md
@@ -0,0 +1,12 @@
+# 用户指引view
+1.使用dialog（推荐）
+
+2.使用WindowManager（因为是基于WindowManager,所以当退出Activity或者按了home键之后需要处理；还有就是无法获取返回键
+
+3.使用自定义View：
+https://github.com/yilylong/UserGuideView
+可定制性不好，UI是死的，还需要单独写，当然可以作为参考写自己想要的效果
+
+![](/demo1.png)</br>
+![](/demo2.png)</br>
+![](/demo3.png)
--- app/src/main/java/com/test/maskdemo/SecondActivity.java
+++ app/src/main/java/com/test/maskdemo/SecondActivity.java
@@ -17,6 +17,9 @@ import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private WindowManager windowManager;
+    private ImageView img;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,20 +31,20 @@ public class SecondActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                initMask2();
+                initMask();
             }
         });
     }
 
     //MaskDemo第二种方式
     private void initMask() {
-        final WindowManager windowManager = getWindowManager();
+        windowManager = getWindowManager();
         // 动态初始化图层
-        final ImageView img = new ImageView(this);
+        img = new ImageView(this);
         img.setLayoutParams(new WindowManager.LayoutParams(
                 android.view.ViewGroup.LayoutParams.MATCH_PARENT,
                 android.view.ViewGroup.LayoutParams.MATCH_PARENT));
-        img.setImageResource(R.mipmap.ic_launcher);
+        img.setImageResource(R.mipmap.test);
         // 设置LayoutParams参数
         WindowManager.LayoutParams params = new WindowManager.LayoutParams();
         //兼容6.0
@@ -70,53 +73,9 @@ public class SecondActivity extends AppCompatActivity {
         });
     }
 
-    //MaskDemo第二种方式
-    // 有可能有这种需求:点击蒙层进行跳转，然后右上角会有取消按钮，点击取消按钮蒙层消失
-    private void initMask2() {
-        View view = LayoutInflater.from(this).inflate(R.layout.layout_dialog_second, null);
-        Window window = getWindow();
-        window.getDecorView().setPadding(0, 0, 0, 0);
-        window.setGravity(Gravity.CENTER);
-        window.setContentView(view);
-        WindowManager.LayoutParams lp = window.getAttributes();
-        lp.width = WindowManager.LayoutParams.FILL_PARENT;
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        window.setAttributes(lp);
-        // 动态初始化图层
-        final ImageView img = (ImageView) view.findViewById(R.id.iv);
-        final ImageView imgExit = (ImageView) view.findViewById(R.id.iv_exit);
-        imgExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SecondActivity.this, "Image dismiss", Toast.LENGTH_SHORT).show();
-            }
-        });
-        img.setImageResource(R.mipmap.test);
-        // 设置LayoutParams参数
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
-        //兼容6.0
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            params.type = WindowManager.LayoutParams.TYPE_TOAST;
-        } else {
-            params.type = WindowManager.LayoutParams.TYPE_PHONE;
-        }
-        // 设置显示格式
-        params.format = PixelFormat.RGBA_8888;
-        // 设置对齐方式
-        params.gravity = Gravity.LEFT | Gravity.TOP;
-        // 设置宽高
-        params.width = WindowManager.LayoutParams.MATCH_PARENT;
-        params.height = WindowManager.LayoutParams.MATCH_PARENT;
-        // 添加到当前的窗口上
-        // 点击图层之后，将图层移除
-        img.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-//                windowManager.removeView(img);
-                Toast.makeText(SecondActivity.this, "img click", Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    protected void onPause() {
+        super.onPause();
+        windowManager.removeView(img);
     }
-
 }
